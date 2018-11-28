63ae1ab3c471c932cffcdce1f2fcfa0a955f74a8
Luke Mondy <luke.mondy@data61.csiro.au>
2018-11-28 16:17:55 +1100

    Change url

--- dimmer_bamboo.sh
+++ dimmer_bamboo.sh
@@ -14,7 +14,7 @@ GREEN_BRIGHTNESS=5
 AUTH_USER=bitbucket_user
 AUTH_PASS=bitbucket_pass
 #REST_URL="bamboo.keg.ertos.in.nicta.com.au:8085/rest/api/latest/result.json"
-REST_URL="bamboo.keg.ertos.in.nicta.com.au:8085/rest/api/1.0/result/L4V.json"
+REST_URL="https://bamboo.ts.data61.csiro.au/rest/api/1.0/result/L4V.json"
 QUERY='.results.result | map(select(.plan.shortKey != "TESTBOARD" and .plan.enabled)) | map(select(has("state") and .plan.enabled)) | map("Successful" == .state) | all'
 BUILD_SUCCESS='true'
 
