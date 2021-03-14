93836a9b68b400866b89a55cec5cdca72ee89967
Peter Chubb <Peter.Chubb@data61.csiro.au>
2021-03-15 09:56:43 +1100

    Adjust for remote lavalamps
    IM&T won't open the ports to allow QBtruck to grab Bamboo status, so
    run this script on ts-data and control the lamps on QBTruck remotely.

--- lavalamps.sh
+++ lavalamps.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Unused.
-#AUTH_USER=bitbucket_user
-#AUTH_PASS=bitbucket_pass
-DIR="`dirname $0`"
+MACHINE=ertos@qbtruck.keg.data61.csiro.au
+
+lavalamps()
+{
+    ssh $MACHINE /usr/local/bin/lavalamps.py "$1"
+}
 
 REST_URL="https://bamboo.ts.data61.csiro.au/rest/api/1.0/result/L4V.json"
 QUERY='.results.result | map(select(.plan.shortKey != "TESTBOARD" and .plan.enabled)) | map(select(has("state") and .plan.enabled)) | map("Successful" == .state) | all'
@@ -11,11 +13,11 @@ BUILD_SUCCESS='true'
 
 BUILD_STATUS=$(curl $REST_URL | jq "$QUERY")
 
-echo "sel4 Hardware-repo regression status:" $BUILD_STATUS
+#echo "sel4 Hardware-repo regression status:" $BUILD_STATUS
 
 if [ x$BUILD_STATUS = x$BUILD_SUCCESS ]; then
-    "$DIR"/lavalamps.py green
+    lavalamps green
 else
-    "$DIR"/lavalamps.py red
+    lavalamps red
 fi
 
